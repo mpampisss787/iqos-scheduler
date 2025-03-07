c1550f5b6d3604286953dd115e3909a10494fcef
mpampisss787 <39789224+mpampisss787@users.noreply.github.com>
2025-03-07 22:52:40 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from my_project.create_app import app  # or from my_project.create_app import app
+from create_app import app  # or from my_project.create_app import app
 
 if __name__ == "__main__":
     app.run()
